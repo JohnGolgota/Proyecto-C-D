219726829c719f286a997e27c17262d49c22fd28
DuvanArwenLazar <3dtuningduvan@gmail.com>
2022-08-15 17:34:12 -0500

    Ya casi lo consigo

--- bd/conexion-php/iniciosesion.php
+++ bd/conexion-php/iniciosesion.php
@@ -1,32 +1,62 @@
 <head></head><body></body>
 <?php
-    session_start();
     include 'conexiondb.php';
+
+    # otro tipo de conexion a una base de datos
+    $dbhost = 'localhost';
+    $dbuser = 'root';
+    $dbpass = '';
+    $dbname = 'base_datos_cd';
 
     # Tambien verificamos que NO este vacio.
     if(!empty($_POST['UsuarioIS']) && !empty($_POST['ContrasenaIS'])){
+        # Conexion.
+        $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
-        $conexion = new Conexion();
-        $conexion->BdConnect();
+        # Verificacion De Errores.
+        if(!$conn){
+            die("Error -> ".mysqli_connect_error);
+        }
 
         $nombre = $_POST['UsuarioIS'];
         $contrasena = $_POST['ContrasenaIS'];
 
-        # Hacemos una consulta.
-        $sql = "SELECT * FROM tbl_usuario WHERE nombre_usr = '.$nombre' AND contrasena_usr = '.$contrasena'";
+        # Hacemos una consulta (con un metodo distinto, que funciona masomenos igual que el otro).
+        $query = mysqli_query($conn, "SELECT * FROM tbl_usuario WHERE nombre_usr = '.$nombre' AND contrasena_usr = '.$contrasena'");
+        $fila = mysqli_num_rows($query);
+        $pssvalida = password_verify($_POST['ContrasenaIS'], $resultado['contrasena_usr']))
 
-        $consulta = $conexion->stm->prepare($sql);
-        $consulta->execute();
-
-        $objeto = $consulta->fetch(PDO::FETCH_OBJ);
-        // var_dump($objeto);
-
-        if($objeto == TRUE){
+        # Si es 1, significa que se encontro un resultado. Lo que sugiere que si esta registrada esa persona.
+        if($fila == 1){
             header('location: ../../index-user');
         } else {
             echo 'no existis capo';
         }
 
+
+        # PRUEBA 2
+        // $conexion = new Conexion();
+        // $conexion->BdConnect();
+
+        // $nombre = $_POST['UsuarioIS'];
+        // $contrasena = $_POST['ContrasenaIS'];
+
+        // # Hacemos una consulta.
+        // $sql = "SELECT * FROM tbl_usuario WHERE nombre_usr = '.$nombre' AND contrasena_usr = '.$contrasena'";
+
+        // $consulta = $conexion->stm->prepare($sql);
+        // $consulta->execute();
+
+        // $objeto = $consulta->fetch(PDO::FETCH_OBJ);
+        // // var_dump($objeto);
+
+        // if($objeto == TRUE){
+        //     header('location: ../../index-user');
+        // } else {
+        //     echo 'no existis capo';
+        // }
+
+        # PRUEBA 1
         // # Creacion y preparamos la sentencia inmediatamente (en la misma linea).
         // $conexion->stm->prepare("SELECT id_usr, nombre_usr, correo_usr, contrasena_usr FROM tbl_usuario WHERE nombre_usr=:UsuarioIS");
         // // $busqueda = $conexion->stm->prepare($consulta); // $consulta->bindParam(':nombre_usr', $_POST['UsuarioIS']); // $busqueda->execute();
