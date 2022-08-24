ca74410053c5c817e16613156cee13c2281c90b3
DuvanArwenLazar <3dtuningduvan@gmail.com>
2022-08-24 17:38:36 -0500

    Pagina finalizada, lista para produccion.

--- Index-user.php
+++ Index-user.php
@@ -34,6 +34,7 @@ foreach ($rowUser as $user) {
   <link rel="stylesheet" href="./Bootstrap/bootstrap.min.css">
 
   <!-- hojas de estilo personalizadas -->
+  <link rel="stylesheet" href="./Estilos/style.css">
   <link rel="stylesheet" href="./Estilos/style-user.css">
 
   <!-- librerias importadas -->
@@ -46,28 +47,28 @@ foreach ($rowUser as $user) {
   <header class="align-items-center navbar navbar-expand-sm position-relative cabeza d-flex">
     <nav class="container-xxl">
       <!-- logo -->
-      <div><a href="./Index-user.html"><img src="./Imagenes/favicon-min.png" alt="Logo" width="35px"></a></div>
+      <section><a href="./Index-user.html"><img src="./Imagenes/favicon-min.png" alt="Logo" width="35px"></a></section>
 
       <!-- botones -->
-      <div class="navbar-nav d-inline align-items-end text-end position-absolute end-0 botones">
+      <section class="navbar-nav d-inline align-items-end text-end position-absolute end-0 botones">
         <!-- Boton "+" -->
-        <div class="nav-item">
-          <button class="boton-mas"> + </button>
-        </div>
+        <section class="nav-item">
+          <button class="boton-mas" style="outline: 1px solid red;"> + </button>
+        </section>
 
         <!-- switch -->
-        <div class="contenido-switch"><label class="switch align-items-center"><input type="checkbox" class="input-banner"><span class="slider"></span></label></div>
+        <section class="contenido-switch"><label class="switch align-items-center"><input type="checkbox" class="input-banner"><span class="slider"></span></label></section>
 
         <!-- Boton user -->
-        <div class="mt-2 usuario">
+        <section class="mt-2 usuario">
           <i class="fa-solid fa-user icono"></i><label for="" class="user-nav"><?php echo "$user->nombre_usr$user->id_usr"; ?></label>
-        </div>
+        </section>
 
         <!-- boton Menu Desplegable. -->
         <button class="boton menu-user" onclick="menuDesplegable();"> ... </button>
         <aside id="menuDesplegable" style="display: none;" class="menu-no">
           <!--  -->
-          <div class="contenido-menu">
+          <section class="contenido-menu">
             <ul>
               <hr class="salto">
               <li><a href="#"><?php echo "$user->nombre_usr"; ?></a></li>
@@ -81,9 +82,9 @@ foreach ($rowUser as $user) {
               <hr class="salto">
               <li><a href="index.html"> Cerrar sesión </a></li>
             </ul>
-          </div>
+          </section>
         </aside>
-      </div>
+      </section>
     </nav>
   </header>
 
@@ -92,7 +93,7 @@ foreach ($rowUser as $user) {
 
     <!--  -->
     <section class="superbox bg-secondary">
-      <h1 class="container-sm top-50">¡¡ESTADISTICAS!!</h1>
+      <h1 class="container-sm top-50">Estamos haciendo la pagina, por favor esperar 3 años masomenos </h1>
 
     </section>
 
@@ -101,18 +102,18 @@ foreach ($rowUser as $user) {
 
       <!--  -->
       <article class="row align-items-center herramienta herramienta-uno">
-        <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
-          <div class="carousel-inner">
-            <div class="carousel-item active">
+        <section id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+          <section class="carousel-inner">
+            <section class="carousel-item active">
               <img src="Imagenes/1.jpg" width="1360px" height="400px" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
+            </section>
+            <section class="carousel-item">
               <img src="Imagenes/2.jpg" width="1360px" height="400px" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
+            </section>
+            <section class="carousel-item">
               <img src="Imagenes/3.jpg" width="1360px" height="400px" class="d-block w-100" alt="...">
-            </div>
-          </div>
+            </section>
+          </section>
           <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
@@ -121,65 +122,65 @@ foreach ($rowUser as $user) {
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
           </button>
-        </div>
+        </section>
       </article>
 
       <!--  -->
       <article class="row align-items-center my-2 herramienta herramienta-dos">
 
         <!--  -->
-        <div class="col">
+        <section class="col">
           <h3>Lorem ipsum dolor sit amet.</h3>
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam voluptas nihil minima odio cum nulla!</p>
-        </div>
+        </section>
 
         <!--  -->
-        <div class="col"><img src="" alt="Agenda"></div>
+        <section class="col"><img src="" alt="Agenda"></section>
       </article>
 
       <!--  -->
       <article class="row align-items-center herramienta herramienta-tres">
 
         <!--  -->
-        <div class="col"><img src="" alt="Agenda"></div>
+        <section class="col"><img src="" alt="Agenda"></section>
 
         <!--  -->
-        <div class="col">
+        <section class="col">
           <h3>Lorem ipsum dolor sit amet.</h3>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat inventore sit hic dignissimos magni pariatur!</p>
-        </div>
+        </section>
       </article>
 
       <!--  -->
       <article class="row align-items-center my-2 herramienta herramienta-cuatro">
 
         <!--  -->
-        <div class="col">
+        <section class="col">
           <h3>Lorem ipsum dolor sit amet.</h3>
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto necessitatibus officia, odio cum blanditiis facilis.</p>
-        </div>
+        </section>
 
         <!--  -->
-        <div class="col"><img src="" alt="Agenda"></div>
+        <section class="col"><img src="" alt="Agenda"></section>
       </article>
     </section>
   </main>
 
   <!-- Remove the containe if you want to extend the Footer to full width. -->
   <!-- Footer -->
-  <div class="container-fluid px-4 mt-3 footer-cd">
+  <section class="container-fluid px-4 mt-3 footer-cd">
 
     <footer class="text-center text-lg-start text-white footer-cd-f">
       <!-- Grid container -->
-      <div class="container p-4 footer-cd-d">
+      <section class="container p-4 footer-cd-d">
         <!--Grid row-->
-        <div class="row my-4">
+        <section class="row my-4">
           <!--Grid column-->
-          <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <section class="col-lg-3 col-md-6 mb-4 mb-md-0">
 
-            <div class="rounded-circle shadow-1-strong d-flex align-items-center justify-content-center mb-4 mx-auto" style="width: 150px; height: 150px;">
+            <section class="rounded-circle shadow-1-strong d-flex align-items-center justify-content-center mb-4 mx-auto" style="width: 150px; height: 150px;">
               <img src="./Imagenes/favicon-min.png" height="70" alt="" loading="lazy" />
-            </div>
+            </section>
 
             <p class="text-center">Carpe Diem</p>
 
@@ -196,11 +197,11 @@ foreach ($rowUser as $user) {
               </li>
             </ul>
 
-          </div>
+          </section>
           <!--Grid column-->
 
           <!--Grid column-->
-          <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <section class="col-lg-3 col-md-6 mb-4 mb-md-0">
             <h5 class="text-uppercase mb-4"> Herramientas </h5>
 
             <ul class="list-unstyled">
@@ -217,11 +218,11 @@ foreach ($rowUser as $user) {
                 <a href="#HerramientaCuatro" class="text-white"><i class="fas fa-seedling pe-3"></i> Hazlo Tu Mismo </a>
               </li>
             </ul>
-          </div>
+          </section>
           <!--Grid column-->
 
           <!--Grid column-->
-          <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <section class="col-lg-3 col-md-6 mb-4 mb-md-0">
             <h5 class="text-uppercase mb-4"> Desarroladores </h5>
 
             <ul class="list-unstyled">
@@ -235,11 +236,11 @@ foreach ($rowUser as $user) {
                 <a href="#!" class="text-white"><i class="fas fa-user pe-3"></i>Ivan</a>
               </li>
             </ul>
-          </div>
+          </section>
           <!--Grid column-->
 
           <!--Grid column-->
-          <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <section class="col-lg-3 col-md-6 mb-4 mb-md-0">
             <h5 class="text-uppercase mb-4"> Contacto </h5>
 
             <ul class="list-unstyled">
@@ -253,24 +254,24 @@ foreach ($rowUser as $user) {
                 <p><i class="fas fa-phone pe-2 mb-0"></i> GitHub -> Proyecto-C-D </p>
               </li>
             </ul>
-          </div>
+          </section>
           <!--Grid column-->
-        </div>
+        </section>
         <!--Grid row-->
-      </div>
+      </section>
       <!-- Grid container -->
 
       <!-- Copyright -->
-      <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+      <section class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
         <a class="text-white" href="#"> Carpe Diem </a>
         © 2022 All Rights Reserved
-      </div>
+      </section>
       <!-- Copyright -->
     </footer>
 
-  </div>
+  </section>
 
-  </div>
+  </section>
 
   <!-- Scripts Inportados: -->
   <!-- Bootstrap -->
