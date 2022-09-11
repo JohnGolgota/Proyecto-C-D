f1db5bca8f34a247ea26832467027ddf96a7d62b
DuvanArwenLazar <3dtuningduvan@gmail.com>
2022-09-11 15:40:22 -0500

    Pomodoro Terminado (sin estilos)

--- Index-user.php
+++ Index-user.php
@@ -93,19 +93,18 @@
 
     <!--  -->
     <section class="superbox bg-secondary calendar-box">
-      <iframe src="https://calendar.google.com/calendar/embed?height=720&wkst=1&bgcolor=%231363df&ctz=America%2FBogota&showTitle=0&showNav=1&src=M2R0dW5pbmdkdXZhbkBnbWFpbC5jb20&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=ZXMuY28jaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&src=ZW4uY28jaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23039BE5&color=%2333B679&color=%230B8043&color=%230B8043" width="1280" height="720" frameborder="0" scrolling="no" class="calendar-content"></iframe>
+      <!-- <iframe src="https://calendar.google.com/calendar/embed?height=720&wkst=1&bgcolor=%231363df&ctz=America%2FBogota&showTitle=0&showNav=1&src=M2R0dW5pbmdkdXZhbkBnbWFpbC5jb20&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=ZXMuY28jaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&src=ZW4uY28jaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23039BE5&color=%2333B679&color=%230B8043&color=%230B8043" width="1280" height="720" frameborder="0" scrolling="no" class="calendar-content"></iframe> -->
     </section>
 
     <!--  -->
     <section class="text-center my-2 herramientas">
 
       <!--  -->
-      <article class="row align-items-center my-2 herramienta herramienta-dos">
+      <article class="row align-items-center my-2 herramienta herramienta-dos herramienta-pomodoro">
 
         <!--  -->
         <section class="col">
-          <h3></h3>
-          <p></p>
+          <input type="text" class="nombre-pomodoro" placeholder="Nombre Actividad" maxlength="10">
         </section>
 
         <!--  -->
--- Scripts/pomodoro-funct.js
+++ Scripts/pomodoro-funct.js
@@ -1,25 +1,27 @@
-var seconds = prompt("Segundos -> ");
-var minutes = prompt("Minutos -> ");
-var pomodoro_timing = setInterval(function(){
-    if(minutes == 0 && seconds == 0){
-        alert("Cuenta Regresiva Terminada");
-        clearInterval(pomodoro_timing); // Detenenemos el SetInterval. 
-    }
+// var seconds = prompt("Segundos -> ");
+// var minutes = prompt("Minutos -> ");
+// var seconds = 60;
+// var minutes = 5;
+// var pomodoro_timing = setInterval(function(){
+//     if(minutes == 0 && seconds == 0){
+//         alert("Cuenta Regresiva Terminada");
+//         clearInterval(pomodoro_timing); // Detenenemos el SetInterval. 
+//     }
 
-    if (seconds == 0){
-        seconds = 60; minutes--;
-    }
+//     if (seconds == -1){
+//         seconds = 59; minutes--;
+//     }
 
-    if(minutes <= 10 && seconds <= 10){
-        console.log(minutes + ":" + seconds); 
-    } else if(seconds < 10 && minutes < 10){
-        console.log("0" + minutes + ":" + "0" + seconds);
-    } else if(seconds <= 10){
-        console.log("0" + minutes + ":" + seconds);
-    } else if(minutes <= 10){
-        console.log(minutes + ":" + "0" + seconds);
-    } 
+//     if(minutes < 10 && seconds < 10){
+//         console.log("0" + minutes + ":" + "0" + seconds); 
+//     } else if(seconds > 9 && minutes > 9){
+//         console.log(minutes + ":" + seconds);
+//     } else if(seconds < 10 && minutes > 10){
+//         console.log(minutes + ":" + "0" + seconds);
+//     } else if(minutes < 10 && seconds > 10){
+//         console.log("0" + minutes + ":" + seconds);
+//     } 
 
-    seconds--;
+//     seconds--;
 
-}, 100);
+// }, 100);
