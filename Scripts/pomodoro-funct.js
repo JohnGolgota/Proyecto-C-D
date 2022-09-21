abc5a6808bf0b5fbfcd98006387f7c563d6096cc
JohnGolgota <JohnGolgota@users.noreply.github.com>
2022-09-21 10:31:53 -0500

    a

--- Scripts/pomodoro-funct.js
+++ Scripts/pomodoro-funct.js
@@ -1,6 +1,7 @@
 // Segundos.
 let seconds = 0;
-let minutes;
+const minutes = document.getElementById("minutes").value;
+console.log(minutes);
 
 // Variable para cambiar los valores.
 let change = 0;
@@ -68,73 +69,73 @@ let cicloPomodoro = 0;
 //     }, 100);
 // }
 
-function pomodoroValues(){
-    // Desde esta funcion, voy a controlar los valores.
+// function pomodoroValues(){
+//     // Desde esta funcion, voy a controlar los valores.
     
-    // return minutes;
-}
+//     // return minutes;
+// }
 
-function pomodoroTimer(){
-    while (change <= 2) {
-        if(change == 0){
-            minutes = document.getElementById("minutes").value;
-            break;
-        } else if(change == 1){
-            minutes = document.getElementById("shortbreak").value;
-            break;
-        } else {
-            minutes = document.getElementById("longbreak").value;
-            break;
-        }
-    }
+// function pomodoroTimer(){
+//     while (change <= 2) {
+//         if(change == 0){
+//             minutes = document.getElementById("minutes").value;
+//             break;
+//         } else if(change == 1){
+//             minutes = document.getElementById("shortbreak").value;
+//             break;
+//         } else {
+//             minutes = document.getElementById("longbreak").value;
+//             break;
+//         }
+//     }
 
-    change++;
+//     change++;
     
-    // Desde esta funcion, voy a hacer el conteo.
-    console.log("Minutos -> ", minutes);
-    console.log("Segundos -> ", seconds);
+//     // Desde esta funcion, voy a hacer el conteo.
+//     console.log("Minutos -> ", minutes);
+//     console.log("Segundos -> ", seconds);
 
-    if(minutes == 0 && seconds == 0){
-        cicloPomodoro += 1;
-        pomodoroAlert();
-    } 
+//     if(minutes == 0 && seconds == 0){
+//         cicloPomodoro += 1;
+//         pomodoroAlert();
+//     } 
     
-    else {
-        // Disminuimos un minuto y reiniciamos los segundos.
-        if (seconds == -1){
-            seconds = 59; minutes--;
-        }
+//     else {
+//         // Disminuimos un minuto y reiniciamos los segundos.
+//         if (seconds == -1){
+//             seconds = 59; minutes--;
+//         }
 
-        // Alguna de esas se cumple SI o SI.
-        if(minutes < 10 && seconds < 10){
-            document.getElementById("timer-funct").innerText = "0" + minutes + ":" + "0" + seconds;
-        } else if(seconds > 9 && minutes > 9){
-            document.getElementById("timer-funct").innerText = minutes + ":" + seconds;
-        } else if(seconds < 10 && minutes > 10){
-            document.getElementById("timer-funct").innerText = minutes + ":" + "0" + seconds;
-        } else if(minutes < 10 && seconds > 10){
-            document.getElementById("timer-funct").innerText = "0" + minutes + ":" + seconds;
-        } 
+//         // Alguna de esas se cumple SI o SI.
+//         if(minutes < 10 && seconds < 10){
+//             document.getElementById("timer-funct").innerText = "0" + minutes + ":" + "0" + seconds;
+//         } else if(seconds > 9 && minutes > 9){
+//             document.getElementById("timer-funct").innerText = minutes + ":" + seconds;
+//         } else if(seconds < 10 && minutes > 10){
+//             document.getElementById("timer-funct").innerText = minutes + ":" + "0" + seconds;
+//         } else if(minutes < 10 && seconds > 10){
+//             document.getElementById("timer-funct").innerText = "0" + minutes + ":" + seconds;
+//         } 
 
-        seconds--;
-    }
-}
+//         seconds--;
+//     }
+// }
 
-function pomodoroAlert(){
-    // Esta funcion, me da la alerta de los descansos.
-    if(cicloPomodoro % 2 != 0){
-        swal({
-            title: "¡Buen Trabajo!",
-            text: "Has hecho bastante, ¡Tomate un descanso!",
-            icon: "success",
-            button: "Vale",
-        });
-    } else if(ciclo % pomodoro == 0){
-        swal({
-            title: "¡De nuevo a la acción!",
-            text: "Ya has descansado, ¡Sigamos!",
-            icon: "success",
-            button: "Vale",
-        });
-    }
-}
+// function pomodoroAlert(){
+//     // Esta funcion, me da la alerta de los descansos.
+//     if(cicloPomodoro % 2 != 0){
+//         swal({
+//             title: "¡Buen Trabajo!",
+//             text: "Has hecho bastante, ¡Tomate un descanso!",
+//             icon: "success",
+//             button: "Vale",
+//         });
+//     } else if(ciclo % pomodoro == 0){
+//         swal({
+//             title: "¡De nuevo a la acción!",
+//             text: "Ya has descansado, ¡Sigamos!",
+//             icon: "success",
+//             button: "Vale",
+//         });
+//     }
+// }
