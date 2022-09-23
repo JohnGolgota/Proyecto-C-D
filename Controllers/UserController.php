e476f1f7b87d773184a079e8af827611b6904726
unknown <SENA@MEDDFPCPDFSPB83.FormacionSena.RED>
2022-09-22 20:42:18 -0500

    redirect logico

--- Controllers/UserController.php
+++ Controllers/UserController.php
@@ -224,7 +224,7 @@ if (isset($_GET['action']) && $_GET['action'] == 'abort') {
 // Action vista predefinida
 if(isset($_GET) || !isset($_GET)){
     $usercontroler = new UserController();
-    $usercontroler->VistaIndex();
+    $usercontroler->RedirigirNoUsuario();
     return;
 }
 
