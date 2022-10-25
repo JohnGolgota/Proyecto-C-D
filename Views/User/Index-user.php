79710acb45dabd5a6a1d1a5009e41eb72738338c
DuvanArwenLazar <3dtuningduvan@gmail.com>
2022-10-25 13:11:37 -0500

    Commit

--- Views/User/Index-user.php
+++ Views/User/Index-user.php
@@ -373,6 +373,6 @@ include '../Inc/header.php';
 
 </section>
 </section>
-
-<script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ="crossorigin="anonymous"></script>
+<!-- <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ="crossorigin="anonymous"></script> -->
 <?php include '../Inc/footer-user.php'; ?>
